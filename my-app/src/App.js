0695743114fd518f89eddfa411892e5a1466f21e
Athena <athena.partch@gmail.com>
2021-01-20 12:56:43 -0800

    added styling to li items

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import * as R from "ramda";
 import { useEffect, useState } from "react";
 import "./App.css";
+import styled from "styled-components"
 async function getData(url, setData) {
   const response = await fetch(url);
   const data = await response.json();
@@ -18,16 +19,52 @@ function App() {
   const username = R.path(["username"], data);
   const poses = R.path(["poses"], data);
   return (
-    <section>
+    <Style>
       <ul>
         {R.map(
           pose => (
-            <li>{R.prop("name", pose)} {R.prop("image", pose)}</li>
+            <li>
+              <p>
+              {R.prop("name", pose)} 
+              </p>
+              <p>
+              {R.prop("image", pose)}
+              </p>
+              </li>
           ),
           poses
         )}
       </ul>
-    </section>
+    </Style>
   );
 }
 export default App;
+/*
+remove bullets - done
+outline the cards - done
+give the cards a width and height - done
+give cards a drop shadow - done
+add flexbox to wrap cards - done
+give the ul a max width - done
+center the name - done
+put the name at the top of the card
+*/
+const Style = styled.section`
+color: blue;
+ul {list-style-type: none;
+    display: flex;
+    width: 150px
+    outline: 1px solid black;
+    flex-wrap: wrap;
+  }
+li {outline: 5px solid green;
+    width: 250px; 
+    height: 70px;
+    margin: 25px 25px 30px 30px;
+    box-shadow: 8px 8px 5px #888888;
+    p{text-align: center;
+      margin: 0;}
+  }
+  
+
+`
