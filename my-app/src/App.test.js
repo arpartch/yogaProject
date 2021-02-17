faafeba6601611f91d32285a08bfb53a38e2efa9
Athena <athena.partch@gmail.com>
2021-02-17 12:34:45 -0800

    test  passed

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,10 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import * as R from "ramda"
 import App from './App';
 
-const capitalizeAll = () => ["FOO", "BAR"]
+const capitalizeAll = (words) => words.map(word => R.toUpper(word))
 
 test('capitalizeAll returns a capitalized list of strings', () => {
   const expectedResult = ["FOO", "BAR"]
-  const result = capitalizeAll()
+  const words = ["foo", "bar"]
+  const result = capitalizeAll(words)
+  expect (result).toEqual(expectedResult)
+});
+
+
+test('capitalizeAll returns a different capitalized list of strings', () => {
+  const expectedResult = ["BAR", "FOO"]
+  const words = ["bar", "foo"]
+  const result = capitalizeAll(words)
   expect (result).toEqual(expectedResult)
 });
