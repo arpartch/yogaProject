cc6a0196022202ec9c592100488d4d78e64a6a8c
Athena <athena.partch@gmail.com>
2021-02-17 12:25:29 -0800

    app js test passed

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,10 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const capitalizeAll = () => ["FOO", "BAR"]
+
+test('capitalizeAll returns a capitalized list of strings', () => {
+  const expectedResult = ["FOO", "BAR"]
+  const result = capitalizeAll()
+  expect (result).toEqual(expectedResult)
 });
